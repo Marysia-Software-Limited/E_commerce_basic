df57c1c655eaa919e7f5e7edf54b595764ec11ec
monikamatis <monikamatis@gmail.com>
2023-01-17 17:07:51 +0000

    modified views.py in 'orders' app to add Stripe checkout session to the order_create; created views.py in 'payment' app and added view 'payment+process' with Stripe Checkout creation;

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -28,10 +29,11 @@ def order_create(request):
 
             # launching asynchronous task - send an email notification
             order_created.delay(order.id, total)
+            # set up the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
 
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
     else:
         form = OrderCreateForm()
 
